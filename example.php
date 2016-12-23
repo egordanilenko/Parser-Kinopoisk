01ea7579b94662df9ffd87599d69ed71cadc51b8
Egor Danilenko <ed@tvip.ru>
2016-12-23 12:24:43 +0300

    Example: remove auth data in example, add catch KinopoiskAccessException

--- example.php
+++ example.php
@@ -7,10 +7,12 @@ include_once ('vendor/autoload.php');
 $memcached = new \Memcached();
 $memcached->addServer('127.0.0.1',11211);
 try{
-    $kinopoisk = new KinopoiskInfo($memcached,1,'dimmduh','gfhjkm03');
+    $kinopoisk = new KinopoiskInfo($memcached);//,1,'dimmduh','gfhjkm03');
 //    echo json_encode($kinopoisk->getTopMovies());
     echo json_encode($kinopoisk->getMovieFromId($id));
 
 }catch (\Eadanilenko\KinopoiskInfo\MovieNotFoundException $e){
+    echo json_encode((object)array('code' => $e->getCode(),'message' => $e->getMessage()));
+}catch (\Eadanilenko\KinopoiskInfo\KinopoiskAccessException $e){
     echo json_encode((object)array('code' => $e->getCode(),'message' => $e->getMessage()));
 }
